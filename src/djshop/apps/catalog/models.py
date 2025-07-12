db36f34a97d75802a6aeecb694d1ad84f3f4b0df
AminGoodarzi <amingoudarzy@yahoo.com>
2025-07-12 17:26:20 +0330

    initiated first app project category with it's db design

---
+++ src/djshop/apps/catalog/models.py
@@ -0,0 +1,14 @@
+from treebeard.mp_tree import MP_Node
+from django.db import models
+# Create your models here.
+class Category(MP_Node):
+    title = models.CharField(max_length=255, db_index=True)
+    description = models.CharField(max_length=2048,null=True , blank=True)
+    is_public = models.BooleanField(default=True)
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.title
+    class Meta:
+        verbose_name = "category"
+        verbose_name_plural = "categories"
