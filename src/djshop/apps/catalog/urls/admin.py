bd8d9b0ebf6c7aeec1481ea0f65747dd6b3d1896
AminGoodarzi <amingoudarzy@yahoo.com>
2025-07-17 12:37:08 +0330

    added create and read end points to category table

---
+++ src/djshop/apps/catalog/urls/admin.py
@@ -0,0 +1,6 @@
+from rest_framework.routers import SimpleRouter
+from djshop.apps.catalog.views.admin import CategoryViewSet
+
+router = SimpleRouter()
+router.register('categories', CategoryViewSet,basename='category')
+urlpatterns = [] + router.urls
